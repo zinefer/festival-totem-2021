421629501848524cf2c697d723629e583aa9c416
zinefer <zinefer@gmail.com>
2021-09-07 20:15:26 -0600

    Init

---
+++ Taskfile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ARDUINO_CLI="./bin/arduino-cli --config-file arduino-cli.yml"
+ARDUINO_CLI_VERSION=$(cat .arduinocliversion)
+
+function install {
+    # apt install python-is-python3
+    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh -s $ARDUINO_CLI_VERSION
+    $ARDUINO_CLI core update-index
+    $ARDUINO_CLI core install esp32:esp32
+    lib-install 'FastLED@3.4.0'
+    lib-install 'Adafruit BNO055@1.5.0'
+}
+
+function lib-install {
+    ARG=$@
+    $ARDUINO_CLI lib install "${ARG}"
+}
+
+function list-boards {
+    $ARDUINO_CLI board list
+}
+
+function compile {
+    $ARDUINO_CLI compile --fqbn esp32:esp32:esp32 .
+}
+
+function help {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
+}
+
+TIMEFORMAT="Task completed in %3lR"
+time ${@:-help}
---
+++ festival-totem-2021.ino
@@ -0,0 +1,45 @@
+// LED Deps
+#include "FastLED.h"
+
+// 9DoF Deps
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup() {
+    setupPositionSensor();
+}
+
+void setupPositionSensor() {
+    /* Initialise the sensor */
+    if(!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while(1);
+    }
+    delay(1000);
+    bno.setExtCrystalUse(true);
+}
+
+void loop() {
+
+}
+
+imu::Vector<3> getPosition() {
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.print(euler.z());
+    Serial.println("");
+
+    return euler;
+}
